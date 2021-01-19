e960b5e72ab6f70b723196a30405959e9963446c
Mark Taylor <mark.taylor0822@gmail.com>
2021-01-19 09:52:32 -0500

    Add documentation, remove logs

--- gobservables.go
+++ gobservables.go
@@ -1,22 +1,19 @@
 package gobservables
 
-import "fmt"
-
-// Observable documentation
+// Observable that dispatches events to Observers.
 type Observable struct {
 	Observers []Observer
 }
 
-// Subscribe documentation
+// Subscribe adds a new Observer to the list of Observers and executes a callback.
 func (o *Observable) Subscribe(callback func(o Observer, payload interface{})) Observer {
-	observer := Observer{ID: len(o.Observers), OnDispatch: callback}
+	observer := Observer{ID: len(o.Observers), observableRef: o, OnDispatch: callback}
 	o.Observers = append(o.Observers, observer)
 	return observer
 }
 
-// Unsubscribe documentation
-func (o *Observable) Unsubscribe(observer Observer, callback func(payload interface{})) {
-	fmt.Println("observers before unsubscribe:", len(o.Observers))
+// Unsubscribe removes the Observer from the list of Observers and executes a callback.
+func (o *Observable) Unsubscribe(observer Observer, callback func()) {
 	for index, tempObserver := range o.Observers {
 		if tempObserver.ID == observer.ID {
 			firstHalf := o.Observers[:index]
@@ -24,18 +21,24 @@ func (o *Observable) Unsubscribe(observer Observer, callback func(payload interf
 			o.Observers = append(firstHalf, secondHalf...)
 		}
 	}
-	callback(len(o.Observers))
+	callback()
 }
 
-// Dispatch documentation
+// Dispatch events to a list of Observers.
 func (o Observable) Dispatch(payload interface{}) {
 	for _, observer := range o.Observers {
 		observer.OnDispatch(observer, payload)
 	}
 }
 
-// Observer documentation
+// Observer to catch events dispatched by the Observable.
 type Observer struct {
-	ID         int
-	OnDispatch func(o Observer, payload interface{})
+	ID            int
+	observableRef *Observable
+	OnDispatch    func(o Observer, payload interface{})
+}
+
+// Unsubscribe removes the observer from the observable's list of observers and executes a callback.
+func (o Observer) Unsubscribe(callback func()) {
+	o.observableRef.Unsubscribe(o, callback)
 }
